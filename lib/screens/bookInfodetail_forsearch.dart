2fb5bd9f639792b78cac7744d68843e7b8b77f08
LabRat404 <44639057+LabRat404@users.noreply.github.com>
2023-01-05 00:45:08 +0800

    remove button auto
    remove chat/trade button if self

--- lib/screens/bookInfodetail_forsearch.dart
+++ lib/screens/bookInfodetail_forsearch.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
+import 'package:trade_app/provider/user_provider.dart';
 import 'package:trade_app/widgets/reusable_widget.dart';
 import 'package:http/http.dart' as http;
 import 'package:url_launcher/url_launcher.dart';
 import 'package:trade_app/screens/showOtherUser.dart';
 import 'package:trade_app/screens/chatter.dart';
+import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:trade_app/provider/user_provider.dart';
+import 'package:trade_app/widgets/reusable_widget.dart';
+import 'package:provider/provider.dart';
+import 'package:trade_app/provider/user_provider.dart';
+import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
+import 'package:trade_app/widgets/nav_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:babstrap_settings_screen/babstrap_settings_screen.dart';
+import 'package:trade_app/screens/information_page.dart';
+import 'package:trade_app/screens/login_page.dart';
+import 'package:trade_app/screens/change_page.dart';
+import 'package:trade_app/screens/avatarchange.dart';
+import 'package:trade_app/widgets/reusable_widget.dart';
+import 'package:trade_app/provider/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class InfoDetailPageSearch extends StatefulWidget {
   final String hashname;
@@ -74,6 +96,7 @@ class _InfoDetailPageSearchState extends State<InfoDetailPageSearch> {
 
   @override
   Widget build(BuildContext context) {
+    var self = context.watch<UserProvider>().user.name;
     return Scaffold(
       appBar: ReusableWidgets.LoginPageAppBar('Book Detailed Info'),
       body: Padding(
@@ -114,27 +137,28 @@ class _InfoDetailPageSearchState extends State<InfoDetailPageSearch> {
                       mainAxisSize: MainAxisSize.min,
                       //alignment: Alignment.center,
                       children: [
-                        ElevatedButton.icon(
-                          icon: Icon(Icons.recycling),
-                          label: Text("Trade with user " + info[3]),
-                          onPressed: () async {
-                            print("Trade!Book hash is " + info[7]);
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                  content: Text('Trade Request Sent!')),
-                            );
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => Chatter(title: "hihi"),
-                              ),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.green,
-                            shadowColor: Colors.orange,
+                        if (info[3] != self)
+                          ElevatedButton.icon(
+                            icon: Icon(Icons.recycling),
+                            label: Text("Trade with user " + info[3]),
+                            onPressed: () async {
+                              print("Trade!Book hash is " + info[7]);
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                    content: Text('Trade Request Sent!')),
+                              );
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => Chatter(title: info[3]),
+                                ),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.green,
+                              shadowColor: Colors.orange,
+                            ),
                           ),
-                        ),
                         ElevatedButton.icon(
                           icon: Icon(Icons.link),
                           label: Text("Show more on Google Play Book"),
